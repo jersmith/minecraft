537066d0bbe605bf6c604f09d9ae5f5d9bb6aad6
Mike Agney <magney@gmail.com>
2022-07-08 15:44:44 -0700

    Adds foundation and create subcommands
    The following commands are added:
    
    * `/mazegen foundation [depth] [width]`
      Creates a foundation of blocks starting under the player's
      feet, extending `depth` blocks ahead of them and `width` blocks
      to the right. `depth` and `width` must be at least 3.
      If the player is holding a placeable block, the foundation will
      be made from it; otherwise, the foundation will be bedrock.
    
    * `/mazegen create`
      Attempts to create a maze using the block the player is looking
      at as a foundation. It will try to find the biggest rectangle of
      contiguous squares made of the same material (the `foundation`
      subcommand is a good way to create such a thing).
      The maze will be made of the same material as the foundation and
      extend two squares high. Existing blocks inside the maze area
      will be destroyed, except for doors on the boundary of the rectangle.
      Such doors will be preserved and torches placed alongside them.

--- MazeGenerator/src/main/java/com/karbonhq/games/minecraft/mazegenerator/MazeGeneratorPlugin.java
+++ MazeGenerator/src/main/java/com/karbonhq/games/minecraft/mazegenerator/MazeGeneratorPlugin.java
@@ -4,23 +4,15 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.karbonhq.games.minecraft.mazegenerator.commands.MazegenCommand;
+
 public final class MazeGeneratorPlugin extends JavaPlugin {
 	@Override
 	public void onEnable() {
+		this.getCommand("mazegen").setExecutor(new MazegenCommand(this));
 	}
 	
 	@Override
 	public void onDisable() {
-	}
-	
-	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
-	{
-		if (cmd.getName().equalsIgnoreCase("mazegen"))
-		{
-			sender.sendMessage("Congratulations, you invoked mazegen! It does nothing yet, though.");
-			return true;
-		}
-		return false;
 	}
 }
